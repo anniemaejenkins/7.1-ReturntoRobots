ccd0c2910c91eef8558abcb8c8fd1ad74e9d3df4
Annie-mae Jenkins <annie-maejenkins@Annie-maes-MacBook-Pro.local>
2017-07-05 16:01:49 -0400

    inital commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const MongoClient = require('mongodb').MongoClient
+, assert = require('assert');
+const data = require('./data');
+const express = require('express');
+const app = express();
+const mustacheExpress = require('mustache-express');
+
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+
+app.get('/', function (req, res) {
+  res.render('list', data);
+});
+
+
+
+
+MongoClient.connect('mongodb://localhost:27017/newdb', (error, db) => {
+  const robots = db.collection('users');
+  assert.equal(null, error);
+  console.log("connected successfully to server");
+
+  robots.find({}).toArray((error, results) => {
+
+  });
+
+  robots.findOne({}, (error, results) => {
+    console.log(results);
+  });
+
+});
+
+
+
+
+app.listen(3000);
